ee5891c95270c398184840136e2949639919962c
onli <onli89@googlemail.com>
2014-05-07 00:08:09 +0200

    Replace autoconf-buildsystem with pkg-config/Makefile The existing build-system was too complicated for me, and I was unable to fix a compile bug in Ubuntu 14.04. In preparation for this fix, the buildystem now is as easy as possible (and hopefully still portable enough).

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the simdock package
-#
-0 4	* * *	root	simdock_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(simdock):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="simdock" command="/usr/bin/simdock"
